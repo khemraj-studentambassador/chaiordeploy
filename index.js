3b51bfb3a117e6c5eea628a6bffd1df60d9f78f0
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2023-11-29 22:19:11 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+// const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("hello World!")
+})
+
+app.get('/test', (req, res) => {
+    res.send("<i>Hey, there</i>");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
